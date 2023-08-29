a54b0c8ca2de1b236a88eaab27225175be3a2bf6
Maria <mgordienkom@gmail.com>
2023-08-29 23:46:17 +0200

    add home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PaintingRepository;
+use App\Repository\EventPhotoRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(PaintingRepository $paintingRepository, EventPhotoRepository $photoRepository): Response
     {
+        $paintings = $paintingRepository->findBy(
+            ['enable' => 'true'],
+            ['name' => 'ASC']
+        );
+        $photos = $photoRepository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'paintings'     => $paintings,
+            'photos'     => $photos,
         ]);
     }
 }
