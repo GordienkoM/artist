c12c3e201a15d78b995afca25f3dee427add8605
Maria <mgordienkom@gmail.com>
2023-09-04 18:21:08 +0200

    add admin menue and painting admin config

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Event;
+use App\Entity\Category;
+use App\Entity\Painting;
+use App\Entity\EventPhoto;
+use App\Entity\PaintingImage;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -34,12 +39,17 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Artist');
+            ->setTitle('Администрация');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Панель управления', 'fa fa-home');
+        yield MenuItem::linkToCrud('Картины', 'fa fa-picture-o', Painting::class);
+        yield MenuItem::linkToCrud('Категории', 'fa fa-list-alt', Category::class);
+        yield MenuItem::linkToCrud('Форографии картин', 'fas fa-camera-retro', PaintingImage::class);
+        yield MenuItem::linkToCrud('События', 'fa fa-calendar', Event::class);
+        yield MenuItem::linkToCrud('Форографии событий', 'fas fa-camera', EventPhoto::class);
+
     }
 }
--- src/Controller/Admin/PaintingCrudController.php
+++ src/Controller/Admin/PaintingCrudController.php
@@ -3,23 +3,58 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Painting;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PaintingCrudController extends AbstractCrudController
 {
+    public const GALLERY_IMAGE_BASE_PATH = 'assets/img/galleryImages';
+    public const GALLERY_IMAGE_UPLOAD_DIR = 'public/assets/img/galleryImages';
+    
     public static function getEntityFqcn(): string
     {
         return Painting::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Картины')
+            ->setEntityLabelInSingular('картину')
+            ->setPageTitle("index", "Картины")
+            ->setPaginatorPageSize(10);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            ImageField::new('gallery_image','Фото в галерее')               
+            ->setBasePath(self::GALLERY_IMAGE_BASE_PATH)
+            ->setUploadDir(self::GALLERY_IMAGE_UPLOAD_DIR)
+            ->setSortable(false),
+            TextField::new('name','Название'),
+            TextField::new('nameEn','Название англ.'),
+            IntegerField::new('number','Номер'),
+            NumberField::new('price','Цена'),
+            IntegerField::new('creationYear','Год создания'),
+            NumberField::new('hight','Высота')->setNumDecimals(1),
+            NumberField::new('width','Ширина')->setNumDecimals(1),
+            TextField::new('material','Матерьял'),
+            TextField::new('alt','Описание фото')->hideOnIndex(),
+            TextField::new('altEn','Описание фото англ.')->hideOnIndex(),
+            BooleanField::new('enable','Актив.'),
+            ArrayField::new('images','Изображения')->onlyOnIndex(),
+            AssociationField::new('images','Изображения')->hideOnIndex(),
+
         ];
     }
-    */
 }
