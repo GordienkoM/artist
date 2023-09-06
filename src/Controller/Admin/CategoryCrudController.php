e0f7b5ddce01678dd13de04097a4cc405089c4d3
Maria <mgordienkom@gmail.com>
2023-09-07 00:55:37 +0200

    crud category

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class CategoryCrudController extends AbstractCrudController
@@ -12,14 +16,23 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInPlural('Категории картин')
+            ->setEntityLabelInSingular('категорию картин')
+            ->setPageTitle("index", "Категории картин")
+            ->setPaginatorPageSize(10);
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('Name','Название'),
+            TextField::new('NameEn','Название перевод'),
+            BooleanField::new('enable','Актив.'),
         ];
     }
-    */
 }
