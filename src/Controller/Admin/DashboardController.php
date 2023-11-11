dc1e90f434ba88737bec17bfd9cd08aa7c778b2a
Maria <mgordienkom@gmail.com>
2023-11-11 18:49:29 +0100

    submenu for paintings

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,6 +10,7 @@ use App\Entity\PaintingImage;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Controller\Admin\PaintingCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -20,11 +21,11 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-       
+
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        $url=$adminUrlGenerator->setController(PaintingCrudController::class)->generateUrl();
+        $url = $adminUrlGenerator->setController(PaintingCrudController::class)->generateUrl();
         return $this->redirect($url);
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
@@ -47,12 +48,15 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Панель управления', 'fa fa-home');
-        yield MenuItem::linkToCrud('Картины', 'fa fa-picture-o', Painting::class);
+        //yield MenuItem::linkToDashboard('Панель управления', 'fa fa-home');
+        yield MenuItem::subMenu('Картины', 'fa fa-picture-o')->setSubItems([
+            MenuItem::linkToCrud('Добавить картину', 'fas fa-plus', Painting::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Посмотреть картины', 'fas fa-eye', Painting::class)
+        ]);
+        // yield MenuItem::linkToCrud('Картины', 'fa fa-picture-o', Painting::class);
         yield MenuItem::linkToCrud('Категории', 'fa fa-list-alt', Category::class);
         yield MenuItem::linkToCrud('Форографии картин', 'fas fa-camera-retro', PaintingImage::class);
         yield MenuItem::linkToCrud('События', 'fa fa-calendar', Event::class);
         yield MenuItem::linkToCrud('Форографии событий', 'fas fa-camera', EventPhoto::class);
-
     }
 }
